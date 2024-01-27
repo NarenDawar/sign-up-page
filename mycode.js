53b9fcec3b9611691e6b95d3f8a727f3c99d551d
Naren <148577200+NarenDawar@users.noreply.github.com>
2024-01-27 16:11:26 -0500

    Finished product, removed the odin logo. Will return to it later.

--- mycode.js
+++ mycode.js
@@ -1,10 +1,15 @@
 let pass = document.getElementById("pass");
 let cpass = document.getElementById("cpass");
+let message = document.getElementById("message");
 
-if(pass === cpass) {
-    pass.set
-}
+var check = function() {
+    if(document.getElementById("pass").value == document.getElementById("cpass").value) {
+        document.getElementById("message").style.color = "green";
+        document.getElementById("message").innerHTML = "Matching";
+    }
 
-else {
-
+    else {
+        document.getElementById("message").style.color = "red";
+        document.getElementById("message").innerHTML = "Not matching";
+    }
 }
